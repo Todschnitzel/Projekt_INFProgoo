a4f14ab51862c68352aa29abfdb8c86c05992212
nilss <nilss@169.254.83.52>
2018-03-07 12:04:16 +0100

    lighthouse

--- Projekt_Breakout/src/breakout/LightHouseView.java
+++ Projekt_Breakout/src/breakout/LightHouseView.java
@@ -3,7 +3,7 @@ package breakout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
-import java.lang.reflect.Array;
+import java.io.IOException;
 
 import javax.swing.JButton;
 
@@ -12,11 +12,13 @@ import acm.graphics.GObject;
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
+import de.cau.infprogoo.lighthouse.LighthouseDisplay;
 
-public class DesktopView extends GraphicsProgram {
+public class LightHouseView extends GraphicsProgram {
+	private LighthouseDisplay display = new LighthouseDisplay("Todschnitzel", "API-TOK_iFNS-m9w4-g6zN-0jNd-g3tJ");
 	/* Fenstergröße */
-	private static final int APPLICATION_WIDTH = 400;
-	private static final int APPLICATION_HEIGHT = 600;
+	private static final int APPLICATION_WIDTH = 28;
+	private static final int APPLICATION_HEIGHT = 14;
 	/* Objekte des Spiels */
 	private GObject ball;
 	private GRect paddle;
@@ -24,36 +26,32 @@ public class DesktopView extends GraphicsProgram {
 	/* Modell */
 	private Model model;
 	/* Objekt Variablen */
-	private static final int PADDLE_WIDTH = 60;
-	private static final int PADDLE_HEIGHT = 10;
-	private static final int PADDLE_Y_OFFSET = 30;
+	private static final int PADDLE_WIDTH = 3;
+	private static final int PADDLE_HEIGHT = 1;
+	private static final int PADDLE_Y_OFFSET = 0;
 
-	private static final int BALL_RADIUS = 10;
+	private static final int BALL_RADIUS = 1;
 
-	private static final double BRICK_WIDTH = 40;
-	private static final double BRICK_HEIGHT = 10;
-	private static final double SEPERATION = 5;
+	private static final double BRICK_WIDTH = 2;
+	private static final double BRICK_HEIGHT = 1;
+	private static final double SEPERATION = 1;
 	private static int ACTUAL_SCORE = 0;
 	private RandomGenerator rgen = new RandomGenerator();
-
 	/**
-	 * Zeichnet einen Ball.
-	 * 
-	 * @return Returns a Ball Objekt.
+	 * Zeichnet ein Level.
+	 * @param levelOne Level zu zeichnen.
+	 */
+	private void drawBricks(double[][] levelOne) {
+		// TODO Auto-generated method stub
+		
+	}
+	/**
+	 * Zeichnet den Ball.
 	 */
 	private void drawBall() {
-		double x = model.getX();
-		double y = model.getY();
-		GRect ballFirst = new GRect(x, y, BALL_RADIUS, BALL_RADIUS);
-		ballFirst.setFilled(true);
-		ball = ballFirst;
-		add(ball);
+		
 	}
-
-	/**
-	 * Zeichnet ein Paddle.
-	 */
-	public void drawPaddle() {
+	private void drawPaddle() {
 		double x = getWidth() / 2 - PADDLE_WIDTH / 2;
 		double y = getHeight() - PADDLE_Y_OFFSET - PADDLE_HEIGHT;
 		paddle = new GRect(x, y, PADDLE_WIDTH, PADDLE_HEIGHT);
@@ -61,23 +59,11 @@ public class DesktopView extends GraphicsProgram {
 		add(paddle);
 		addMouseListeners();
 	}
-
-	public void drawBricks(double[][] level) {
-
-		for (int i = 0; i < level.length; i++) {
-			GRect brick = new GRect(level[i][0], level[i][1] + 30, BRICK_WIDTH, BRICK_HEIGHT);
-			brick.setFilled(true);
-			Color rdm = rgen.nextColor();
-			brick.setFillColor(rdm);
-			brick.setColor(rdm);
-			add(brick);
-		}
-	}
-
+	
+	
 	/*------------------- CONTROLLER------------------ */
 
 	public void run() {
-		resize(400, 600);
 		model = new Model(APPLICATION_WIDTH, APPLICATION_HEIGHT, BALL_RADIUS);
 		setupGame();
 	}
